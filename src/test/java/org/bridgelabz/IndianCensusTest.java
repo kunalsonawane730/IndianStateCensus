7cabbe7b4c8a2ce7257c6eb2bc16723c5336fb87
Kunal <kunalsonawane730@gmail.com>
2022-12-15 07:37:41 +0530

    Add: analyze Indian State code from perticular CSV file

--- src/test/java/org/bridgelabz/IndianCensusTest.java
+++ src/test/java/org/bridgelabz/IndianCensusTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Assertions;
 
 public class IndianCensusTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "IndiaStatesCensusData.csv";
-    public IndianCensusAnalyser analyser;
+    IndianCensusAnalyser analyser=new IndianCensusAnalyser();
+    private static final String INDIAN_CSV_STATE_PATH = "StateCodes.csv";
 
     @Test
     public void returnSize() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -19,7 +19,6 @@ public class IndianCensusTest {
     @Test
     public void shouldExecuteProperly_IfRecordsAreEqual() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -29,7 +28,6 @@ public class IndianCensusTest {
     @Test
     public void shouldReturnRecordsif_CSVFileIsDetected() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -39,7 +37,6 @@ public class IndianCensusTest {
     @Test
     public void shouldReturnCustomException_ifDelimiterIncorrect() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
@@ -49,8 +46,57 @@ public class IndianCensusTest {
     @Test
     public void shouldReturCustomException_ifCSVHeaderIncorrect() throws Exception {
         try {
-            analyser = new IndianCensusAnalyser();
             Assertions.assertEquals(29, analyser.loadCensusData(INDIA_CENSUS_CSV_FILE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//	State
+
+    @Test
+    public void returnSizeState() throws Exception {
+        try {
+
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldExecuteProperly_IfRecordsAreEqualState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldReturnRecordsif_CSVFileIsDetectedState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData("xyz.csv"));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldReturnCustomException_ifDelimiterIncorrectState() throws Exception {
+        try {
+
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
+        }catch(CustomException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldRetureCustomException_ifCSVHeaderIncorrectState() throws Exception {
+        try {
+            Assertions.assertEquals(37, analyser.loadStateData(INDIAN_CSV_STATE_PATH));
         }catch(CustomException e) {
             e.printStackTrace();
         }
