e599a28c57bcd54a712f71cbca0290e66d254c14
Kunal <kunalsonawane730@gmail.com>
2022-12-15 06:55:07 +0530

    Add: welcome message for Indian State Census Analyser Program

---
+++ src/main/java/org/bridgelabz/App.java
@@ -0,0 +1,9 @@
+package org.bridgelabz;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Indian State Census Analyser ");
+    }
+}
